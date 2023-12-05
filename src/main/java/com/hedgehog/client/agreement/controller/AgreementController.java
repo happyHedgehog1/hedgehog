334bf7db7857d40a12ddfa2f35ddb0a15d84e21c
jiseon <jiseonu650@naver.com>
2023-12-05 16:58:27 +0900

    "2023.12.05 4:57pm jiseon footer 이용약관 , 개인정보처리 , 공지사항 연결 완"

--- src/main/java/com/hedgehog/client/agreement/controller/AgreementController.java
+++ src/main/java/com/hedgehog/client/agreement/controller/AgreementController.java
@@ -1,7 +1,22 @@
 package com.hedgehog.client.agreement.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/agreement/*")
 public class AgreementController {
+
+    @GetMapping("/privacyPolicy")
+    public String getPrivacyPolicy() {
+
+        return "client/content/agreement/privacyPolicy";
+    }
+
+    @GetMapping("/termsAndConditions")
+    public String getTermsAndConditions() {
+
+        return "client/content/agreement/termsAndConditions";
+    }
 }
