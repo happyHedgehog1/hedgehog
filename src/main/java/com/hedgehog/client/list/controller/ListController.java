4b164d39ffa5be1192c9208377f2096ab91df5f0
jiseon <jiseonu650@naver.com>
2023-12-06 17:40:00 +0900

    "2023.12.06 5:40pm jiseon 마이바티스 폴더생성 및 yml설정값 작성 및 List클래스 진행 중"

--- src/main/java/com/hedgehog/client/list/controller/ListController.java
+++ src/main/java/com/hedgehog/client/list/controller/ListController.java
@@ -1,15 +1,55 @@
 package com.hedgehog.client.list.controller;
+import com.hedgehog.client.list.model.dto.ListDTO;
+import com.hedgehog.client.list.model.sercive.ListServiceImpl;
+import lombok.Value;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 @Controller
-@RequestMapping("/list/*")
+@RequestMapping("/list")
 @Slf4j
 public class ListController {
 
-//    @GetMapping("category")
+    @Value("${im")
+    private String IMAGE_DIR;
+
+    @Value("${spring.servlet.multipart.location}")
+    private String ROOT_LOCATION;
+
+    private final ListServiceImpl listServiceImpl;
+
+    public ListController(ListServiceImpl listServiceImpl) {
+        this.listServiceImpl = listServiceImpl;
+    }
+
+    @GetMapping("/list")
+    public ModelAndView selectCategoryList(ModelAndView mv) {
+
+        log.info("");
+        log.info("");
+        log.info("[ListController] ========================== start");
+
+        List<ListDTO> category = listServiceImpl.selectCategoryList();
+
+        log.info("category : " + category);
+
+        mv.addObject("category", category);
+        mv.setViewName("client/list/category");
+
+        log.info("[ListController] ========================== end");
+
+        return mv;
+
+    }
+
+
+
+//    @GetMapping("/category")
 //    public String getCategoryLocation(@RequestParam String type, Model model) {
 //
 //        log.info("[ListController] getCategoryLocation type : {}", type);
@@ -31,7 +71,7 @@ public class ListController {
 //        return "category";
 //
 //    }
-    // 데이터베이스에서 필요정보를 객체로 받아서 결과페이지로 넘겨준다.
+     데이터베이스에서 필요정보를 객체로 받아서 결과페이지로 넘겨준다.
 
     @GetMapping("/category")
     public String getCategory(){
