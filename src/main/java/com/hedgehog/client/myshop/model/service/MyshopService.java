64ef3e2c61d031e5ffec03f8261c8730d49de12b
JunKyeong-Lee <worldreaming@gmail.com>
2023-12-11 18:03:34 +0900

    회원정보수정, 회원가입, 나의정보 이메일API 연결 제외 완료.

--- src/main/java/com/hedgehog/client/myshop/model/service/MyshopService.java
+++ src/main/java/com/hedgehog/client/myshop/model/service/MyshopService.java
@@ -2,8 +2,10 @@ package com.hedgehog.client.myshop.model.service;
 
 import com.hedgehog.client.auth.model.dto.MemberDTO;
 import com.hedgehog.client.myshop.model.dao.MyshopMapper;
+import com.hedgehog.common.common.exception.UserEmailNotFoundException;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class MyshopService {
@@ -19,5 +21,27 @@ public class MyshopService {
 
     public MemberDTO getMemberInfo(int userCode) {
         return myshopMapper.getMemberInfo(userCode);
+    }
+
+    public String getEmail(int userCode) throws UserEmailNotFoundException {
+        String email = myshopMapper.getEmail(userCode);
+        if (email == null) {
+            throw new UserEmailNotFoundException("이메일을 찾지 못했습니다.");
+        }
+        return email;
+    }
+
+    @Transactional
+    public boolean modifyMember(MemberDTO member) {
+        /*Member를 modify 하려면 다음과 같은 과정을 거쳐야 한다.*/
+
+        // userId로 값 가져오기.
+        int result1 = myshopMapper.updateUser(member);
+        int userCode = myshopMapper.getUserCode(member.getUserId());
+        System.out.println(userCode);
+        int result2 = myshopMapper.updateCustomer(userCode, member);
+        int result3 = myshopMapper.updateMember(userCode, member);
+        int result = result1 + result2 + result3;
+        return result >= 0 ? true : false;
     }
 }
