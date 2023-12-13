eedc6facfd2d38c573cfc54bea2b7f365ce6314c
JunKyeong-Lee <worldreaming@gmail.com>
2023-12-13 12:57:45 +0900

    공지사항, 리뷰, 1:1문의 게시판 제작. faq제작 준비중

---
+++ src/main/java/com/hedgehog/client/board/model/dto/ReviewDTO.java
@@ -0,0 +1,25 @@
+package com.hedgehog.client.board.model.dto;
+
+import lombok.*;
+
+import java.sql.Timestamp;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class ReviewDTO {
+    private int reviewCode; // tbl_review의 review_code 리뷰번호
+    private int productCode; // tbl_review의 product_code 제품번호
+    private String productName; // tbl_product의 product_name
+    private String productImgPath; // tbl_product_img의 convert_path. 썸네일 이미지의 주소. image_classification이 Thumbnails인 경우만.
+    // 여기에 이제 리뷰 사진이 들어가야 한다.
+    private String optionCode; // tbl_review의 option_code 옵션코드(#000000)
+    private String optionName; // tbl_option의 name 옵션이름(흰색)
+    private int grade; // tbl_review의 grade 별점
+    private String content; // tbl_review의 content 내부내용
+    private Timestamp writeDate; //tbl_review의 write_date 작성시간. -> 수정 불가능하게? -> 가능은 한데 굳이 의미가 있는가.
+    private int memberCode; // tbl_review의 member_code 멤버코드
+    private String memberId; // tbl_user의 id 유저아이디
+}
--- src/main/java/com/hedgehog/client/board/model/service/BoardService.java
+++ src/main/java/com/hedgehog/client/board/model/service/BoardService.java
@@ -1,7 +1,9 @@
 package com.hedgehog.client.board.model.service;
 
 import com.hedgehog.client.board.model.dao.BoardMapper;
+import com.hedgehog.client.board.model.dto.NoticeDTO;
 import com.hedgehog.client.board.model.dto.QuestionDTO;
+import com.hedgehog.client.board.model.dto.ReviewDTO;
 import com.hedgehog.common.paging.SelectCriteria;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -25,11 +27,47 @@ public class BoardService {
         return result;
     }
 
+    public int selectTotalCountReviewList(Map<String, String> searchMap) {
+        int result = mapper.selectTotalCountReviewList(searchMap);
+        log.info("");
+        log.info("");
+        log.info("BoardService : selectTotalCountReviewList ... : " + result);
+
+        return result;
+    }
+
     public List<QuestionDTO> selectQuestionList(SelectCriteria selectCriteria) {
         List<QuestionDTO> result = mapper.selectQuestionList(selectCriteria);
         log.info("");
         log.info("");
         log.info("BoardService : selectQuestionList ... : " + result);
+
+        return result;
+    }
+
+    public List<ReviewDTO> selectReviewList(SelectCriteria selectCriteria) {
+        List<ReviewDTO> result = mapper.selectReviewList(selectCriteria);
+        log.info("");
+        log.info("");
+        log.info("BoardService : selectReviewList ... : " + result);
+
+        return result;
+    }
+
+    public int selectTotalCountNoticeList(Map<String, String> searchMap) {
+        int result = mapper.selectTotalCountNoticeList(searchMap);
+        log.info("");
+        log.info("");
+        log.info("BoardService : selectTotalCountNoticeList ... : " + result);
+
+        return result;
+    }
+
+    public List<NoticeDTO> selectNoticeList(SelectCriteria selectCriteria) {
+        List<NoticeDTO> result = mapper.selectNoticeList(selectCriteria);
+        log.info("");
+        log.info("");
+        log.info("BoardService : selectReviewList ... : " + result);
 
         return result;
     }
