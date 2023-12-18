859e80e38109458c89a66d4790036e0250b99395
jiseon <jiseonu650@naver.com>
2023-12-18 18:25:01 +0900

    12.18 jiseon 상품상세페이지 이미지출력 및 상품명 가격 출력 완 (가격 천단위 콤마표시 진행중)

---
+++ src/main/java/com/hedgehog/client/product/model/dao/ProductInfoMapper.java
@@ -0,0 +1,13 @@
+package com.hedgehog.client.product.model.dao;
+
+import com.hedgehog.client.product.model.dto.ProductDetailDTO;
+import com.hedgehog.client.product.model.dto.ProductTextDTO;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface ProductInfoMapper {
+    List<ProductDetailDTO> selectProductDetail(int number);
+
+}
