9c4b3a7e3e87773327dd849e7532dc223213b162
JunKyeong-Lee <worldreaming@gmail.com>
2023-12-22 13:00:24 +0900

    날짜 검색 수정

--- src/main/java/com/hedgehog/client/orderDetails/model/dto/OrderDTO.java
+++ src/main/java/com/hedgehog/client/orderDetails/model/dto/OrderDTO.java
@@ -3,10 +3,11 @@ package com.hedgehog.client.orderDetails.model.dto;
 import lombok.*;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @AllArgsConstructor@NoArgsConstructor@Getter@Setter@ToString
 public class OrderDTO {
     private String state;
     private LocalDate dateStart;
-    private LocalDate dateEnd;
+    private LocalDateTime dateEnd;
 }
