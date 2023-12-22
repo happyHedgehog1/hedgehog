c68f9e54a7f7db72e1f776ef9c4f900c7a4278c7
jjangjjaeyoung <wkdrkwodud2174@gmail.com>
2023-12-22 18:20:30 +0900

    1222 마무리

--- src/main/java/com/hedgehog/admin/adminStatistics/controller/adminStatisticsController.java
+++ src/main/java/com/hedgehog/admin/adminStatistics/controller/adminStatisticsController.java
@@ -1,14 +1,16 @@
 package com.hedgehog.admin.adminStatistics.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/statistics")
+@Slf4j
 public class adminStatisticsController {
     @GetMapping("/statistics")
     public String statistics(){
-        return "admin/content/statistics/statistics.html";
+        return "admin/content/statistics/statistics";
     }
 }
